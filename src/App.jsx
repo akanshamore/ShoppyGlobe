198fce384e9f0bb7d712e3f6d49dcec7f109fd76
akansha-internshala <akansha.more@outlook.com>
2024-12-17 21:27:14 +0530

    Basic routes set up

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,23 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router";
+const Home = () => {
+  return <h1>Home</h1>;
+};
+
+const Cart = () => {
+  return <h1>Cart</h1>;
+};
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
-    <div>
-      <h1>ShoppyGlobe</h1>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
