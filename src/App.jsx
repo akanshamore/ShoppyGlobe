78ff64b4693d3f5892fb72de9d40960585a9f693
akansha-internshala <akansha.more@outlook.com>
2024-12-25 23:52:35 +0530

    redux example setup

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,35 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter, Route, Routes } from "react-router";
+import { useSelector, useDispatch } from "react-redux";
+import { decrement, increment } from "./redux/slices/counterSlice";
+
 const Home = () => {
-  return <h1>Home</h1>;
+  const count = useSelector((state) => state.counter.value);
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <h1>Home</h1>
+      <div>
+        <div>
+          <button
+            aria-label="Increment value"
+            onClick={() => dispatch(increment())}
+          >
+            Increment
+          </button>
+          <span>{count}</span>
+          <button
+            aria-label="Decrement value"
+            onClick={() => dispatch(decrement())}
+          >
+            Decrement
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 const Cart = () => {
